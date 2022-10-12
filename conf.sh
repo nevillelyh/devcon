86144c7304a95eb49ccc347e9f2e2cdb82d2b473
Neville Li <neville.lyh@gmail.com>
2022-10-12 17:14:53 -0400

    check for BigQuery secret key

--- conf.sh
+++ conf.sh
@@ -12,3 +12,29 @@ POSTGRES_PRIVATE_DATABASES=(hive iceberg)
 POSTGRES_PUBLIC_DATABASES=(pgsql)
 
 TRINO_CATALOGS=(bigquery hive iceberg mysql postgres)
+
+############################################################
+
+bindir="$(dirname "$(readlink -f "$0")")"
+basedir="$(readlink -f "$bindir/..")"
+
+check_biguqery() {
+    for catalog in "${TRINO_CATALOGS[@]}"; do
+        if [ "$catalog" == "bigquery" ] && [ ! -f "$basedir/trino/secrets/bigquery.json" ]; then
+            echo "Missing BigQuery secret key: trino/secrets/bigquery.json"
+            (( errors++ ))
+        fi
+    done
+}
+
+check_all() {
+    errors=0
+    check_biguqery
+    if [ "$errors" -ne 0 ]; then
+        exit 1
+    fi
+}
+
+if [ ! -f /.dockerenv ]; then
+    check_all
+fi
