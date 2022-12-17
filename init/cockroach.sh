e1a4d0960eeb3d0178c43bd0df8211b277e8e9c5
Neville Li <neville.lyh@gmail.com>
2022-12-16 19:23:50 -0500

    make CockroachDB single node

--- init/cockroach.sh
+++ init/cockroach.sh
@@ -7,12 +7,11 @@ source /conf.sh
 
 [[ -f $HOME/init-complete ]] && exit 0
 
-/init/wait-for-it.sh "cockroach1.devcon.io:26257" -s -t 0 -- echo "CockroachDB is up"
+/init/wait-for-it.sh "cockroach.devcon.io:26257" -s -t 0 -- echo "CockroachDB is up"
 
-/cockroach/cockroach init --insecure --host cockroach1.devcon.io
 for database in "${COCKROACH_DATABASES[@]}"; do
     /cockroach/cockroach workload init "$database" \
-        postgresql://root@cockroach1.devcon.io:26257?sslmode=disable
+        postgresql://root@cockroach.devcon.io:26257?sslmode=disable
 done
 
 touch "$HOME/init-complete"
